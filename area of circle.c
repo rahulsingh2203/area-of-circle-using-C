c27c0e41b24b43ae6f46e7496ac608f8ebe8e291
Rahul Singh <rahul.singh.ec.2017@miet.ac.in>
2023-01-10 17:31:20 +0530

    first commit

---
+++ area of circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main(){
+	float r,area,pi=3.14;
+	printf("enter the radius of the circle:");
+	scanf("%f/n", &r);
+	area = pi * r *r;
+	printf("area of Circle is : %.2f",area);
+	
+}
